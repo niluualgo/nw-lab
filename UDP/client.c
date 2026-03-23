b2f320124fa6b9923019607a2f176c1e81cb1e86
NIHAL <niluualgo@gmail.com>
2026-03-24 00:57:18 +0530

    Create client.c

---
+++ UDP/client.c
@@ -0,0 +1,39 @@
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int main() {
+  int sockfd;
+  struct sockaddr_in server_addr, client_addr;
+  socklen_t addr_len;
+  char msg[100];
+
+  sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+  if (sockfd < 0) {
+    printf("Socket creation failed!\n");
+    exit(1);
+  }
+
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = htons(5050);
+  server_addr.sin_addr.s_addr = INADDR_ANY;
+
+  if (bind(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+    printf("Bind failed!\n");
+    exit(1);
+  }
+
+  printf("Server waiting for client...\n");
+  addr_len = sizeof(client_addr);
+
+  recvfrom(sockfd, msg, sizeof(msg), 0, (struct sockaddr*)&client_addr, &addr_len);
+  printf("Client: %s\n", msg);
+
+  strcpy(msg, "Response from UDP server");
+  sendto(sockfd, msg, strlen(msg), 0, (struct sockaddr*)&client_addr, addr_len);
+
+  close(sockfd);
+  return 0;
+}
