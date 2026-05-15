c981c993139e2587978fab10a8242c5866599270
niluualgo <niluualgo@gmail.com>
2026-05-15 22:13:05 +0530

    Update leaky_bucket.c

--- leaky_bucket.c
+++ leaky_bucket.c
@@ -12,7 +12,7 @@ int main() {
 
   int store = 0;
   while (n > 0) {
-    printf("Enter incoming packet size: ");
+    printf("\nEnter incoming packet size: ");
     int incoming;
     scanf("%d", &incoming);
     int rem = size - store;
