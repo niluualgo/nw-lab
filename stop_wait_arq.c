6dae63bde8dd140f94958aff3403939e07eb5f4d
niluualgo <niluualgo@gmail.com>
2026-05-17 10:13:54 +0530

    Create stop_wait_arq.c

---
+++ stop_wait_arq.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+  int n;
+  printf("Enter total no. of frames: ");
+  scanf("%d", &n);
+
+  puts("--- STOP AND WAIT ARQ ---");
+  int i = 0, seq = 0;
+  while(i < n) {
+    printf("\nSending frame %d with seq number %d\n", i+1, seq);
+    printf("Enter ACK received (0/1/-1): ");
+    int ack;
+    scanf("%d", &ack);
+    if(ack == seq) {
+      printf("Frame %d transmitted successfully!\n", i+1);
+      seq = 1 - seq; // toggle seq number
+      i++;
+    }
+    else {
+      printf("ACK error!. Retransmitting frame %d...\n", i+1);
+    }
+  }
+  printf("\nAll frames transmitted successfully!\n");
+}
