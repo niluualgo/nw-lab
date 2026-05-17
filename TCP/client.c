46335b52a3c5b32e5cbcaaa9c2e9566417eb4ab4
niluualgo <niluualgo@gmail.com>
2026-05-17 15:19:13 +0530

    Update client.c

--- TCP/client.c
+++ TCP/client.c
@@ -8,7 +8,7 @@ int main() {
   int fd = socket(AF_INET, SOCK_STREAM, 0);
   struct sockaddr_in client = {
       .sin_family = AF_INET,
-      .sin_port = htons(3003),
+      .sin_port = htons(3004),
   };
 
   connect(fd, (struct sockaddr*)&client, sizeof(client));
